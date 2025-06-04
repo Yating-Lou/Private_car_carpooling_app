93ab5d637a1f5c7b579115bc1b2a13d7c5cbcc96
Yating-Lou <3174147652@qq.com>
2025-06-04 21:42:19 +0800

    初始框架

---
+++ frontend/src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  timeout: 5000,
+});
+
+instance.interceptors.request.use(config => {
+  const token = localStorage.getItem('access_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default instance;

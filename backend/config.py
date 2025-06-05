1a8e53bf0e133fd2cdd19b9f3de756bd68cf4888
raywwj <142644819+raywwj@users.noreply.github.com>
2025-06-05 21:46:48 +0800

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = '123456'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/carpooling_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = '123456'
